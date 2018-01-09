1c35a324b1c14f7f662ea35a237eabee9957ed83
jackhowa <jackhowaa@gmail.com>
2018-01-08 20:50:01 -0600

    Add boostrap react elements

--- src/index.js
+++ src/index.js
@@ -3,16 +3,31 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import {
+  PageHeader,
+  Jumbotron,
+  Button,
+  Image,
+  Col,
+  Grid,
+  Row,
+  Well,
+  Panel
+} from "react-bootstrap";
 
 // main app for rendering to dom
 class Base extends React.Component {
   render() {
     return (
-      <div>
-        <Header />
-        <Main />
-        <Article />
-      </div>
+      <Jumbotron>
+        <Grid>
+          <Row>
+            <Header />
+            <Main />
+            <Article />
+          </Row>
+        </Grid>
+      </Jumbotron>
     );
   }
 }
@@ -21,8 +36,9 @@ class Header extends React.Component {
   render() {
     return (
       <div>
-        <h1>Mr. Newton</h1>
-        <h2>The man who trained thousands</h2>
+        <PageHeader className="primary-green">
+          Mr. Newton <small>The man who trained thousands</small>
+        </PageHeader>
       </div>
     );
   }
@@ -30,7 +46,15 @@ class Header extends React.Component {
 
 class Main extends React.Component {
   render() {
-    return <img src="https://pbs.twimg.com/media/DQpuEelXkAAPjqU.jpg" />;
+    return (
+      <Col md={6} sm={6}>
+        <Image
+          src="https://pbs.twimg.com/media/DQpuEelXkAAPjqU.jpg"
+          rounded
+          responsive
+        />
+      </Col>
+    );
   }
 }
 
@@ -38,15 +62,30 @@ class Article extends React.Component {
   render() {
     return (
       <div>
-        <blockquote>
-          It's nice to be great. But it's far greater to be nice
-        </blockquote>
-        <a
-          href="https://www.runnersworld.com/obituary/legendary-cross-country-coach-joe-newton-dies-at-88"
-          target="_blank"
-        >
-          Read more about his life
-        </a>
+        <Col sm={6} md={6}>
+          <Panel bsStyle="success">
+            <Panel.Heading>
+              <Panel.Title componentClass="h3">My favorite quotes</Panel.Title>
+            </Panel.Heading>
+            <Panel.Body>
+              <blockquote>
+                It's nice to be great. But it's far greater to be nice.
+              </blockquote>
+              <blockquote>
+                However fast your are running - run faster!
+              </blockquote>
+            </Panel.Body>
+            <Panel.Footer>
+              <Button
+                href="https://www.runnersworld.com/obituary/legendary-cross-country-coach-joe-newton-dies-at-88"
+                target="_blank"
+                bsStyle="link"
+              >
+                Read more about his life
+              </Button>
+            </Panel.Footer>
+          </Panel>
+        </Col>
       </div>
     );
   }
